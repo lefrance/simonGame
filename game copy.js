221b503661ae2fc958c2ec54542cb37e95868004
lefrance <67027741+lefrance@users.noreply.github.com>
2022-02-22 00:04:25 -0300

    delete copy game js

--- game copy.js
+++
@@ -1,8 +0,0 @@
-for (var i = 0; i < userClickedPattern.length; i++) {
-  if (userClickedPattern[i] == gamePattern[i]){
-    if (userClickedPattern[lastVar] == gamePattern[lastvar]){
-      nextSequence();
-    }
-  }else{
-       gameOver();
-  }
